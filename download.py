d551a002a6e83f0e23f65911d407ff8efee7215d
Jannic Holzer <jannic.holzer@quantumblack.com>
2023-05-27 21:32:15 +0100

    Add implementation of multithreaded downloader

--- download.py
+++ download.py
@@ -3,15 +3,34 @@ import os
 
 import requests
 
-from .utils import create_image_folder, generate_save_path
-
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
+from utils import create_image_folder, generate_save_path
 
 from pathlib import Path
 
+from queue import Queue
+from threading import Thread
+
 TYPES = {"image/jpeg", "image/png"}
 CLIENT_ID = os.environ["imgur_client_id"]
+
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+def main() -> None:
+    """
+    The main function of the script.
+
+    Fetches a dictionary of image URLs by calling the _get_image_urls() function,
+    then downloads the images and saves them in the "images" directory.
+
+    Returns:
+        None
+    """
+    urls = _get_image_urls()
+    save_path = generate_save_path()
+    _download_images_threaded(urls, save_path)
 
 
 def _get_image_urls() -> dict[str, list[str]]:
@@ -43,8 +62,9 @@ def _get_image_urls() -> dict[str, list[str]]:
     return urls
 
 
-def _download_images(urls: dict[str, list[str]], save_path: Path) -> None:
-    """Downloads images from given urls and saves them at the specified path.
+def _download_images_sequential(urls: dict[str, list[str]], save_path: Path) -> None:
+    """Downloads images from given urls and saves them at the specified path
+    sequentially.
 
     Each image is saved with its unique ID if multiple images belong to the same
     ID. A separate folder is created for each ID that has multiple images.
@@ -65,12 +85,75 @@ def _download_images(urls: dict[str, list[str]], save_path: Path) -> None:
         for index, url in enumerate(urls[id]):
             type = url.rpartition(".")[2]
             if len(urls[id]) > 1:
-                _download_image_sequential(url, file_path / f"{index}.{type}")
+                _download_single_image(url, file_path / f"{index}.{type}")
             else:
-                _download_image_sequential(url, file_path / f"{id}.{type}")
+                _download_single_image(url, file_path / f"{id}.{type}")
 
 
-def _download_image_sequential(url: str, save_path: Path) -> None:
+def _download_images_threaded(urls: dict[str, list[str]], save_path: Path) -> None:
+    """Downloads images from given urls and saves them at the specified path
+    using ten threads.
+
+    Each image is saved with its unique ID if multiple images belong to the same
+    ID. A separate folder is created for each ID that has multiple images.
+
+    Args:
+        urls (dict[str, list[str]]): A dictionary containing unique IDs as keys
+            and corresponding lists of image urls as values.
+        save_path (Path): The path where the images should be saved.
+
+    Returns:
+        None
+    """
+    queue = Queue()
+    threads = []
+    for _ in range(10):
+        t = Thread(target=_download_images_worker, args=(queue,))
+        t.start()
+        threads.append(t)
+
+    for id in urls:
+        file_path = save_path
+        if len(urls[id]) > 1:
+            file_path = save_path / id
+            create_image_folder(file_path)
+        for index, url in enumerate(urls[id]):
+            type = url.rpartition(".")[2]
+            if len(urls[id]) > 1:
+                queue.put((url, file_path / f"{index}.{type}"))
+            else:
+                queue.put((url, file_path / f"{id}.{type}"))
+
+    # add sentinels for each thread
+    for _ in threads:
+        queue.put(None)
+
+    queue.join()
+
+    for t in threads:
+        t.join()
+
+
+def _download_images_worker(queue: Queue) -> None:
+    """Worker function for a thread that downloads urls stored in a Queue.
+
+    Args:
+        queue (Queue): A queue object that contains the image urls.
+
+    Returns:
+        None
+    """
+    while True:
+        next_item = queue.get()
+        if next_item is None:
+            queue.task_done()
+            break
+        url, file_path = next_item
+        _download_single_image(url, file_path)
+        queue.task_done()
+
+
+def _download_single_image(url: str, file_path: Path) -> None:
     """Downloads an image from a given url and saves it at the specified path.
 
     Args:
@@ -86,24 +169,9 @@ def _download_image_sequential(url: str, save_path: Path) -> None:
     except requests.HTTPError as http_error:
         logger.error(f"Error downloading url {url}:\n {http_error}")
         return False
-    with open(save_path, "wb") as file:
+    with open(file_path, "wb") as file:
         file.write(response.content)
-    logger.info(f"Successfully downloaded image {url}")
-
-
-def main() -> None:
-    """
-    The main function of the script.
-
-    Fetches a dictionary of image URLs by calling the _get_image_urls() function,
-    then downloads the images and saves them in the "images" directory.
-
-    Returns:
-        None
-    """
-    urls = _get_image_urls()
-    save_path = generate_save_path()
-    _download_images(urls, save_path)
+    logger.info(f"Successfully downloaded {url}")
 
 
 if __name__ == "__main__":
