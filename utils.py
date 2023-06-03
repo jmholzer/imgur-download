ca47883ed52a921fc5f0c30d36280df6b4a3cce6
Jannic Holzer <jannic.holzer@quantumblack.com>
2023-06-03 18:14:50 +0100

    Remove generate_save_path and create_image_folder

--- utils.py
+++ utils.py
@@ -1,39 +1,7 @@
 import logging
-import os
-from datetime import datetime
-from pathlib import Path
 from time import perf_counter
 
 logger = logging.getLogger("main")
-
-
-def create_image_folder(save_path: Path) -> None:
-    """Creates a new folder at the specified path if it doesn't already exist.
-
-    Args:
-        save_path (Path): The path where the new folder should be created.
-
-    Returns:
-        None
-    """
-    os.makedirs(save_path, exist_ok=True)
-
-
-def generate_save_path(tag: str) -> Path:
-    """Generates a save path for images with a unique identifier based on the
-    current timestamp.
-
-    Args:
-        tag (str): The tag to be used for the image search.
-
-    Returns:
-        Path: The path object representing the newly created directory.
-    """
-    save_path = Path("images") / (
-        f"{tag}_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}"
-    )
-    create_image_folder(save_path)
-    return save_path
 
 
 def timer(func: callable) -> callable:
